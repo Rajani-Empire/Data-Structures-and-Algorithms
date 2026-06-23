e871b3ac1b8a412b51f1e39d068f4551e2b4ebfa
Rajani Empire <rajani.swe.sust@gmail.com>
2026-06-23 19:39:16 +0600

    triangle pattern

---
+++ triangle_pattern.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+
+    int n =4;
+    for(int i =1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+
+        }
+        cout<<endl;
+    }
+
+    cout << endl;
+
+   
+    for(int i =1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+
+        }
+        cout<<endl;
+    }
+
+    char ch = 'A';
+    for(int i =1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<ch<<" ";
+
+        }
+        cout<<endl;
+        ch++;
+    }
+
+
+    cout << endl;
+
+   
+    for(int i =1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+
+        }
+        cout<<endl;
+    }
+
+
+
+
+
+    return 0;
+
+}
