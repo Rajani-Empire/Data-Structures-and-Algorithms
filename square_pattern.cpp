868df8a76bcfdba6bf5ab00219a8df9561f6a798
Rajani Empire <rajani.swe.sust@gmail.com>
2026-06-23 19:12:03 +0600

    update square_pattern.cpp

--- square_pattern.cpp
+++ square_pattern.cpp
@@ -9,8 +9,21 @@ int main(){
     
     for(int i =1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            cout<<j<<" ";
+            cout<<"* ";
 
+        }
+        cout<<endl;
+    }
+
+
+    cout << endl;
+
+
+     for(int i =1;i<=n;i++){
+        char ch ='A';
+        for(int j=1;j<=n;j++){
+            cout<< ch << " ";
+            ch++;
         }
         cout<<endl;
     }
