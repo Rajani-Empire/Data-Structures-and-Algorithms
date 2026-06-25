2805a93672cd048ae190afe240bbdf157db61dd6
Rajani Empire <rajani.swe.sust@gmail.com>
2026-06-25 23:54:33 +0600

    day2

---
+++ ternarry_statement.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+  int n;
+  cin>>n;
+
+  cout<<(n>=0 ? "positive" :"negative")<<endl;
+
+
+
+    return 0;
+}
