20a8d67ee4c13cd763bbe333af9f50b4b0c94f60
Rajani Empire <rajani.swe.sust@gmail.com>
2026-06-25 23:47:21 +0600

    day2

---
+++ lower_upper_case.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+
+    char ch;
+    cin>>ch;
+
+    if(ch>='a' && ch<='z'){
+        cout<<"lowercase"<<endl;
+    }
+    else{
+         cout<<"uppercase"<<endl; 
+    }
+
+    return 0;
+
+}
