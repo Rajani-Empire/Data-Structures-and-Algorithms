d10e50ac9d09e42495565d50e1ea367f301cd993
Rajani Empire <rajani.swe.sust@gmail.com>
2026-06-26 16:00:59 +0600

    modified

--- binary.cpp
+++ binary.cpp
@@ -9,19 +9,25 @@ int main(){
    cin>>n;
 
    int rem=0;
-   vector<int>vec1;
+   int ans=0;
+   int pow=1;
+//    vector<int>vec1;
 
    while(n !=0){
     rem = n%2;
-    vec1.push_back(rem);
+    // vec1.push_back(rem);
     n=n/2;
+    ans+=(rem*pow);
+    pow=pow*10;
+
    }
 
-   for(int i=vec1.size()-1;i>=0;i--){
-    cout<< vec1[i];
-   }
+//    for(int i=vec1.size()-1;i>=0;i--){
+//     cout<< vec1[i];
+//    }
 
-
+ cout<<ans;
+ 
 
     return 0;
 }
